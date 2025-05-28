c118b77595d5999fbf708de87b566144123353df
Wontae Jang <wtjang@i-sens.com>
2025-05-28 18:10:46 +0900

    feat: router 설정

--- blog-test/src/router/index.js
+++ blog-test/src/router/index.js
@@ -2,11 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/router_test/'),
   routes,
-  scrollBehavior (to, from, savedPosition) {
-    return { top: 0 }
-  },
 })
 
 export default router
--- blog-test/vite.config.js
+++ blog-test/vite.config.js
@@ -4,5 +4,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/router_test/'
+  base: '/router_test/',
+  build: {
+    rollupOptions: {
+      output: {
+        sanitizeFileName: (name) => {
+          // Sanitizes file names generated during the build process:
+          // - Replaces spaces with dashes ('-').
+          // - Removes invalid characters that are not alphanumeric, underscores (_), periods (.), or dashes (-).
+          return name
+            .replace(/\s+/g, '-') // Replaces spaces with dashes.
+            .replace(/[^a-zA-Z0-9_.-]/g, ''); // Removes all invalid characters.
+        },
+      },
+    }
+  },
 })
